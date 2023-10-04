e06377acc48ae67883d77ceef180239acf3260a4
Habibul Islam <habibulislam6862@gmail.com>
2023-10-04 16:25:40 +0600

    Database class and its methods are implemented

---
+++ lib/Database.php
@@ -0,0 +1,63 @@
+<?php
+
+class Database {
+    public $host = DB_HOST;
+    public $dbName = DB_NAME;
+    public $dbUser = DB_USER;
+    public $dbPass = DB_PASS;
+
+    public $connection;
+    public $error;
+    public $error_no;
+
+    public function __construct(){
+        $this -> connectDB();
+    }
+
+    private function connectDB(){
+        if(!isset($connection)){
+            $this -> connection = new mysqli($this -> host, $this -> dbUser, $this -> dbPass, $this -> dbName);
+        }
+
+        if($this -> connection -> connect_errno){
+            $this -> error = $this -> connection -> connect_error;
+            $this -> error_no = $this -> connection -> connect_errno;
+            die("Error Database Connection");
+        }
+    }
+
+    //insert or update data
+    public function insert($query, $markerDataTypes = "", $values = []){
+        $queryStatement = $this -> connection -> prepare($query);
+        $queryStatement -> bind_param($markerDataTypes, ...$values);
+        return $queryStatement->execute();
+    }
+
+    //select data
+    public function select($query, $markerDataTypes = "", $values = []){
+        $queryStatement = $this -> connection -> prepare($query);
+        $queryStatement -> bind_param($markerDataTypes, ...$values);
+        $result = $queryStatement -> execute();
+        if($result -> num_rows > 0){
+            return $result;
+        } else {
+            return false;
+        }
+    }
+
+    //delete data
+    public function deleteWithId($table, $id ){
+        $query = sprintf(
+            "DELETE FROM %s WHERE id = %s", 
+            $this -> connection -> real_escape_string($table),
+            $this -> connection -> real_escape_string((string)$id)
+        );
+        $result = $this -> connection -> query($query);
+        if($result == false){
+            echo "Error cannot delete id '" . $id . "' from table '" . $table . "'.";
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
