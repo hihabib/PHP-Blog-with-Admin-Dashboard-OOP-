075fecef8351b5053f3da375d8d5a6257ea8a118
Habibul Islam <habibulislam6862@gmail.com>
2023-10-05 06:59:44 +0600

    comments added in logout.php file

--- admin/logout.php
+++ admin/logout.php
@@ -1,9 +1,18 @@
 <?php
+// Check if the 'logout' parameter is set in the URL query string
 if(isset($_GET['logout'])) {
+    // Include the Session class for managing sessions
     include_once("../lib/Session.php");
+    
+    // Call the destroy method of the Session class to end the user's session
     Session::destroy();
+    
+    // Terminate the script execution
     die();
 } else {
+    // If 'logout' parameter is not set, include the configuration file
     include_once("../config/config.php");
+    
+    // Redirect the user to the homepage specified in the 'HOMEPAGE' constant
     header("Location: ".HOMEPAGE);
 }
