94b2ab94fcc6d80803c2a5c73b306c94f354dc9e
Habibul Islam <habibulislam6862@gmail.com>
2023-10-10 01:41:39 +0600

    Comprehensive comments added

--- config/config.php
+++ config/config.php
@@ -1,8 +1,14 @@
 <?php
-define("DB_HOST", 'localhost');
-define("DB_USER", 'root');
-define("DB_PASS", '');
-define("DB_NAME", 'stand-blog');
-define("TBL_CATEGORY", 'categories');
-define("TBL_POST", 'posts');
-define("HOMEPAGE", "http://localhost/stand-blog");
+// Define database connection constants
+define("DB_HOST", 'localhost');  // The hostname or IP address of your database server
+define("DB_USER", 'root');       // The username to connect to the database
+define("DB_PASS", '');           // The password for the database user
+define("DB_NAME", 'stand-blog'); // The name of the database you want to connect to
+
+// Define table names
+define("TBL_CATEGORY", 'categories'); // The name of the table for categories
+define("TBL_POST", 'posts');          // The name of the table for posts
+
+// Define the homepage URL
+define("HOMEPAGE", "http://localhost/stand-blog"); // The URL of your website's homepage
+?>
